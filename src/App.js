52ba886de481f67b6a94e0fc93387908aa502359
mccarlosen <mccarlos.en@gmail.com>
2021-05-26 11:55:03 -0500

    add clean completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ import logo from "./assets/logo.svg";
 
 function App(props) {
 	const [todos, setTodos] = React.useState(props.todos);
-	const [isSelectedAll, setIsSelectedAll] = React.useState(false);
+	const [hasSelected, setHasSelected] = React.useState(false);
+	const [todosFiltered, setTodosFiltered] = React.useState(todos);
+	const [tab, setTab] = React.useState(null);
+
+	React.useEffect(() => {
+		if (tab === null) {
+			setTodosFiltered(todos);
+		} else {
+			setTodosFiltered(todos.filter((todo) => todo.status === tab));
+		}
+	}, [todos, tab]);
+
+	const handleFilter = (filter) => {
+		setTab(filter);
+		if (filter === null) {
+			setTodosFiltered(todos);
+		} else {
+			setTodosFiltered(todos.filter((todo) => todo.status === filter));
+		}
+	};
 
 	const handleAddTodo = (e) => {
 		const value = e.target.value;
@@ -32,29 +51,18 @@ function App(props) {
 		checkSelectedAll(oldTodos);
 	};
 
-	const handleToggleSelectAll = (e) => {
-		const oldTodos = [...todos];
-		if (e.target.checked) {
-			oldTodos.map((todo) => (todo.status = true));
-		} else {
-			oldTodos.map((todo) => (todo.status = false));
-		}
-		setTodos(oldTodos);
-		checkSelectedAll(oldTodos);
-	};
-
 	const handleClearAll = () => {
 		const oldTodos = [...todos];
 		const newTodos = oldTodos.filter((todo) => todo.status !== true);
 		setTodos(newTodos);
-		setIsSelectedAll(false);
+		setHasSelected(false);
 	};
 
 	const checkSelectedAll = (todos) => {
-		if (todos.filter((todo) => todo.status).length === todos.length) {
-			setIsSelectedAll(true);
+		if (todos.filter((todo) => todo.status).length) {
+			setHasSelected(true);
 		} else {
-			setIsSelectedAll(false);
+			setHasSelected(false);
 		}
 	};
 
@@ -72,10 +80,18 @@ function App(props) {
 							type="text"
 							className="h-14 w-full text-color1-200 text-xl px-4 rounded bg-color6-600 transition duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-color1-400 focus:border-transparent"
 							autoFocus
-							placeholder="Insert you text"
+							placeholder="Enter your task"
 						/>
 					</div>
-					<TodoList todos={todos} handleStatus={handleStatus} handleToggleSelectAll={handleToggleSelectAll} handleClearAll={handleClearAll} isSelectedAll={isSelectedAll} />
+					<TodoList
+						todos={todos}
+						todosFiltered={todosFiltered}
+						handleStatus={handleStatus}
+						handleClearAll={handleClearAll}
+						handleFilter={handleFilter}
+						hasSelected={hasSelected}
+						tab={() => tab}
+					/>
 				</div>
 			</section>
 		</div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,25 +2,7 @@ import React from "react";
 import Todo from "./Todo";
 import ButtonsFilter from "./ButtonsFilter";
 
-function TodoList({ todos, handleStatus, handleToggleSelectAll, handleClearAll, isSelectedAll }) {
-	const [todosFiltered, setTodosFiltered] = React.useState(todos);
-	const [tab, setTab] = React.useState(null);
-
-	React.useEffect(() => {
-		if (tab === null) {
-			setTodosFiltered(todos);
-		} else {
-			setTodosFiltered(todos.filter((todo) => todo.status === tab));
-		}
-		if (todos.length === 0) {
-			setTab(null);
-		}
-	}, [todos, tab]);
-
-	const handleFilter = (filter) => {
-		setTab(filter);
-	};
-
+function TodoList({ todos, todosFiltered, handleStatus, handleClearAll, handleFilter, hasSelected, tab }) {
 	const messages = (tab) => {
 		let message;
 		switch (tab) {
@@ -40,7 +22,7 @@ function TodoList({ todos, handleStatus, handleToggleSelectAll, handleClearAll, 
 	if (!todosFiltered.length) {
 		return (
 			<React.Fragment>
-				<div className="flex justify-center text-color6-400 bg-color6-500 rounded-b-md p-4">{messages(tab)}</div>
+				<div className="flex justify-center text-color6-400 bg-color6-500 rounded-b-md p-4">{messages(tab())}</div>
 				<ButtonsFilter
 					countAll={todos.length}
 					countActive={todos.filter((todo) => todo.status === false).length}
@@ -53,19 +35,13 @@ function TodoList({ todos, handleStatus, handleToggleSelectAll, handleClearAll, 
 		return (
 			<React.Fragment>
 				<div className="flex flex-col px-4 mb-3">
-					<div className="flex justify-between text-color5-500">
-						<label>
-							<input id="select-all" type="checkbox" onChange={(e) => handleToggleSelectAll(e)} checked={isSelectedAll} className="css-checkbox lrg" />
-							<label htmlFor="select-all" className="css-label lrg vlad">
-								Select All
-							</label>
-						</label>
-						{isSelectedAll && (
+					<div className="flex justify-end text-color5-500">
+						{hasSelected && tab() !== false && (
 							<button
 								onClick={() => handleClearAll()}
 								className="px-2 py-1 text-xs text-black bg-color8-500 transition duration-150 ease-in-out hover:bg-color8-900 hover:text-color1-100 rounded"
 							>
-								Clear All
+								Clean Completed
 							</button>
 						)}
 					</div>
